72acf84bb39be9ed00940e18c46252f3bf586950
Therasson <therassonkonan@gmail.com>
2023-07-17 23:15:55 +0000

    Initialisation firebase, material design

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'coding-challenge-8ab23',
+    appId: '1:161171187452:web:a5658e6fb53b114a04d979',
+    storageBucket: 'coding-challenge-8ab23.appspot.com',
+    apiKey: 'AIzaSyAUjMEVWxgVmW3Xj5skMaa5RjChJb7lPVk',
+    authDomain: 'coding-challenge-8ab23.firebaseapp.com',
+    messagingSenderId: '161171187452',
+  },
+};
